2b725526ad8e5ec2b95b0c310c31dd1196798aa6
David <gelpiorama@gmail.com>
2019-05-31 23:32:24 +0200

    inicio proyecto

---
+++ src/main/java/org/lasencinas/fastbreaktruck/fastbreaktruck/FastBreakTruck.java
@@ -0,0 +1,13 @@
+package org.lasencinas.fastbreaktruck.fastbreaktruck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastBreakTruck {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastBreakTruck.class, args);
+	}
+
+}
---
+++ src/test/java/org/lasencinas/fastbreaktruck/fastbreaktruck/FastBreakTruckTests.java
@@ -0,0 +1,16 @@
+package org.lasencinas.fastbreaktruck.fastbreaktruck;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FastBreakTruckTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
